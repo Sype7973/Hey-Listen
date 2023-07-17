50c1bd212c94c564c352fc8fffeb73fb497d6066
unknown <adam.day7973@gmail.com>
2023-07-17 20:28:57 +1000

    updated app.js and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+// This is the main file for the portfolio website. It is the first file that is run when the website is loaded.
+import React, { useState, useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-// import ReactDOM from 'react-dom';
+import { useLocation } from 'react-router-dom';
+import './App.css';
 
-// v18 react-dom/server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-  <ChakraProvider>
-    <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</ChakraProvider>
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
-);
+// function App() {
+//   const [currentPath] = useState(window.location.pathname);
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     console.log('currentPath', currentPath);
+//     console.log('location', location);
+//   }, [currentPath, location]);
+
+//   return (
+//     <ApolloProvider client={client}>
+//       <ChakraProvider>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </ChakraProvider>
+//     </ApolloProvider>
+//   );
+// }
