093d721a03c74d584d2b3855e0fdb937ee8bfbcf
Elibrer <eli.brer@gmail.com>
2023-07-18 19:34:45 +0930

    Changed resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,7 +10,7 @@ const typeDefs = gql`
         userType: String
         bio: String
         profilePicture: String
-        musicLinks: String
+        musicLinks: [String]
         posts: [Post]
     }
 
