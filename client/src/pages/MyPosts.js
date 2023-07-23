73900745bfc0567a103f845041e759f92b0de733
unknown <adam.day7973@gmail.com>
2023-07-23 14:08:07 +1000

    updated posts, and remove button

--- client/src/pages/MyPosts.js
+++ client/src/pages/MyPosts.js
@@ -1,130 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { Box, Flex, Card, CardBody, Heading } from "@chakra-ui/react";
+import React from "react";
 import { useQuery } from "@apollo/client";
+import { Box, Flex, Card, CardBody, Heading } from "@chakra-ui/react";
 import { GET_ME } from "../utils/queries";
 import spinner from "../assets/images/spinner.gif";
 import Posts from "../components/Posts";
+import Auth from "../utils/auth";
 
 const MyPosts = () => {
   const { loading, data } = useQuery(GET_ME);
+  const user = data?.me || {};
+  const loggedInUsername = Auth.loggedIn() ? Auth.getProfile().data.username : null;
 
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    if (data && data.me !== null) {
-      setUser(data.me);
-    }
-  }, [data]);
-
-  if (isLoading || loading) {
-    return (
-      <Flex
-        minHeight="100vh"
-        alignItems="center"
-        bg="teal.500"
-        direction="column"
-        justifyContent="center"
-      >
-        <Card m="auto" width="40vw" h="40vw">
-          <CardBody display="flex" alignItems="center" justifyContent="center">
-            <img src={spinner} alt="loading" />
-          </CardBody>
-        </Card>
-      </Flex>
-    );
+  if (loading) {
+    return <img src={spinner} alt="loading" />;
   }
 
-  // Filter the user's posts into "My Posts" and "Accepted Posts"
-  const myPosts = user ? user.posts.filter((post) => !post.isAccepted) : [];
-  const acceptedPosts = user ? user.posts.filter((post) => post.isAccepted) : [];
+  console.log("User Data:", user);
+  console.log("Logged In Username:", loggedInUsername);
+
+  // Filter the user's posts into "My Posts" (Posts belonging to the logged-in user)
+  const myPosts = user.posts ? user.posts.filter((post) => post.username === loggedInUsername) : [];
+
+  console.log("My Posts:", myPosts);
 
   return (
-    <>
-      {user ? (
-        <Box>
-          {/* Section for "My Posts" */}
-          {myPosts.length > 0 && (
-            <Flex
-              p={5}
-              shadow="md"
-              borderWidth="1px"
-              flex="1"
-              borderRadius="md"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-              textAlign="center"
-              bg="gray.50"
-              m={5}
-            >
-              <Heading as="h2" mb={4}>
-                My Posts
-              </Heading>
-              <Posts posts={myPosts} />
-            </Flex>
-          )}
-
-          {/* Section for "Accepted Posts" */}
-          {acceptedPosts.length > 0 ? (
-            <Flex
-              p={5}
-              shadow="md"
-              borderWidth="1px"
-              flex="1"
-              borderRadius="md"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-              textAlign="center"
-              bg="gray.50"
-              m={5}
-            >
-              <Heading as="h2" mb={4}>
-                Accepted Posts
-              </Heading>
-              <Posts posts={acceptedPosts} />
-            </Flex>
-          ) : (
-            <Flex
-              p={5}
-              shadow="md"
-              borderWidth="1px"
-              flex="1"
-              borderRadius="md"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-              textAlign="center"
-              bg="gray.50"
-              m={5}
-            >
-              <Heading as="h2" mb={4}>
-                No Accepted Posts
-              </Heading>
-            </Flex>
-          )}
-        </Box>
+    <Box>
+      {myPosts.length > 0 ? (
+        <Flex
+          p={5}
+          shadow="md"
+          borderWidth="1px"
+          flex="1"
+          borderRadius="md"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          bg="gray.50"
+          m={5}
+        >
+          <Heading as="h2" mb={4}>
+            My Posts
+          </Heading>
+          <Posts posts={myPosts} />
+        </Flex>
       ) : (
         <Flex
-          minHeight="100vh"
+          p={5}
+          shadow="md"
+          borderWidth="1px"
+          flex="1"
+          borderRadius="md"
+          flexDirection="column"
           alignItems="center"
-          bg="teal.500"
-          direction="column"
           justifyContent="center"
+          textAlign="center"
+          bg="gray.50"
+          m={5}
         >
-          <Card m="auto" width="40vw" h="40vw">
-            <CardBody display="flex" alignItems="center" justifyContent="center">
-              <img src={spinner} alt="loading" />
-            </CardBody>
-          </Card>
+          <Heading as="h2" mb={4}>
+            No Posts Found
+          </Heading>
         </Flex>
       )}
-    </>
+    </Box>
   );
 };
 
