61ea849fda3ab3de2d7c12593de2ce884cf702cc
unknown <adam.day7973@gmail.com>
2023-07-25 19:16:57 +1000

    updated filter, still not working

--- client/src/components/PostFilter.js
+++ client/src/components/PostFilter.js
@@ -2,44 +2,29 @@ import React, { useState } from "react";
 import { Select } from "@chakra-ui/react";
 
 const PostFilter = ({ onFilterChange }) => {
-  const [userTypeFilter, setUserTypeFilter] = useState("all");
   const [postTypeFilter, setPostTypeFilter] = useState("all");
-
-  const handleUserTypeFilterChange = (event) => {
-    setUserTypeFilter(event.target.value);
-    onFilterChange(event.target.value, postTypeFilter);
-  };
 
   const handlePostTypeFilterChange = (event) => {
     setPostTypeFilter(event.target.value);
-    onFilterChange(userTypeFilter, event.target.value);
+    onFilterChange(event.target.value); // Pass only the postTypeFilter value
   };
 
-  console.log(userTypeFilter);
-    console.log(postTypeFilter);
+  console.log("postTypeFilter: ", postTypeFilter);
 
   return (
     <div>
-      <Select value={userTypeFilter} onChange={handleUserTypeFilterChange}>
-        <option value="all">All Users</option>
-        <option value="Artists">Artists</option>
-        <option value="Producers">Producers</option>
-      </Select>
-
       <Select value={postTypeFilter} onChange={handlePostTypeFilterChange}>
         <option value="all">All Posts</option>
-        <option value="Looking for Artists">Looking for Artists</option>
-        <option value="Looking for Producers">Looking for Producers</option>
-        <option value="Looking for Samples">Looking for Samples</option>
-        <option value="Looking for Mixing">Looking for Mixing</option>
-        <option value="Looking for Mastering">Looking for Mastering</option>
-        <option value="Looking for Vocals">Looking for Vocals</option>
-        <option value="Looking for Guitarists">Looking for Guitarists</option>
-        <option value="Looking for Bassists">Looking for Bassists</option>
-        <option value="Looking for Drummers">Looking for Drummers</option>
-        <option value="Looking for Pianists">Looking for Pianists</option>
-        <option value="Looking for Synths">Looking for Synths</option>
-        <option value="Looking for Other">Looking for Other</option>
+        <option value="Samples">Looking for Samples</option>
+        <option value="Mixing">Looking for Mixing</option>
+        <option value="Mastering">Looking for Mastering</option>
+        <option value="Vocals">Looking for Vocals</option>
+        <option value="Guitarists">Looking for Guitarists</option>
+        <option value="Bassists">Looking for Bassists</option>
+        <option value="Drummers">Looking for Drummers</option>
+        <option value="Pianists">Looking for Pianists</option>
+        <option value="Synths">Looking for Synths</option>
+        <option value="Other">Looking for Other</option>
       </Select>
     </div>
   );
